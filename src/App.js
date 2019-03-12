838faec03ca6dd4541fd226c74d50a40e52e9920
shoaibmal <shoaib_573@yahoo.co.in>
2019-03-12 16:51:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ export default () => (
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
+  <Link to="/test">Test</Link>
+  
       </nav>
       <div className="content">
         <Routes />
