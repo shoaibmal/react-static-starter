4d7457a679bb4b82ab90d70b710dc4e693f5eed1
shoaibmal <shoaib_573@yahoo.co.in>
2019-04-23 18:20:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,28 @@ import './app.css'
 export default () => (
   <Router>
     <div>
-      <nav>
+  
+   <nav id="nav-wrap">
+
+         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+         <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" class="nav">
+            <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+            <li><a class="smoothscroll" href="#about">About</a></li>
+            <li><a class="smoothscroll" href="#resume">Resume</a></li>
+            <li><a class="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
+            <li><a class="smoothscroll" href="#contact">Contact</a></li>
+         </ul> <!-- end #nav -->
+
+      </nav> <!-- end #nav-wrap -->
+   <!--    <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
-  <Link to="/test">Test</Link>
   
-      </nav>
+      </nav> -->
       <div className="content">
         <Routes />
       </div>
